10ff0b24fb2e4d1ced74d839dee477c1d8e26015
splat46 <stijnhuiskes@gmail.com>
2022-09-01 07:51:32 +0200

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,16 +2,28 @@
     import Table from "./lib/Table.svelte";
 </script>
 
-<main class="min-h-full">
-    <div class="flex flex-col justify-center items-center max-w-4xl mx-auto h-full m-12 text-slate-300">
-        <h1 class="text-4xl mb-6">Draggable Table</h1>
-
-        <div class="bg-slate-800 w-full pb-12 rounded-2xl overflow-hidden">
-            <Table />
+<main class="min-h-full flex justify-center items-center">
+    <div class="container">
+        <div class="flex flex-col w-full max-w-4xl mx-auto text-slate-300 font-mono">
+            <div class="md:w-2/3 mb-8 text-slate-400 drop-shadow-xl shadow-primary">
+                <h1 class="title">Draggable Table</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing, velit in imperdiet ultrices.
+                </p>
+            </div>
+    
+            <div class="relative bg-slate-800 w-full rounded-sm">
+                <div class="absolute -inset-2 bg-gradient-to-r from-secondary via-primary to-secondary opacity-10 blur-2xl"></div>
+                <div class="relative bg-slate-800 z-10 pb-4 overflow-x-auto rounded-md">
+                    <Table />
+                </div>
+            </div>
         </div>
     </div>
 </main>
 
 <style>
-
+.title {
+    @apply bg-gradient-to-r from-secondary/90 via-primary to-secondary bg-clip-text text-4xl tracking-tight text-transparent mb-4;
+}
 </style>
